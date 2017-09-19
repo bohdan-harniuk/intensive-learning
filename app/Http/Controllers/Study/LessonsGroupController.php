3accef63c3d32470e5575bec3c7e964be824dd05
Yurii Voytkov <smartmanyurezzz@gmail.com>
2017-09-19 21:59:31 +0300

    add-exercises

--- app/Http/Controllers/Study/LessonsGroupController.php
+++ app/Http/Controllers/Study/LessonsGroupController.php
@@ -19,10 +19,6 @@ class LessonsGroupController extends Controller
         return view('study.lessons_group.create');
     }
 
-    public function slider(){
-        return view('study.lessons_group.slider');
-    }
-
     public function store(Request $request)
     {
         LessonsGroup::create($request->all());
